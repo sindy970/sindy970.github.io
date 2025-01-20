47593f312693981e3e2766ed3924d2c7c76a4c4d
seunghee.han <seunghee.han@emotion.co.kr>
2025-01-20 19:44:58 +0900

    kakao map 이미지 노출 수정

--- src/pages/Navermaps.js
+++ src/pages/Navermaps.js
@@ -2,50 +2,36 @@ import React from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk"
 import Lovelocation from "../lovelocation.png";
 
-// const { kakao } = window;
-
 const Navermaps = () => {
 
-    const options = {
-        src: {Lovelocation}, // 마커이미지의 주소입니다
-        size: {
-            width: 45,
-            height: 45,
-        }
-    };
-
-    // useEffect(() => {
-    //     const container = document.getElementById('map');
-    //     //지도를 담을 영역의 DOM 레퍼런스
-    //     const options = {
-    //         center: new kakao.maps.LatLng(37.516081,126.722443),
-    //         Level: 3
-    //     };
-    //     const map = new kakao.maps.Map (container, options);
-    //     //지도 생성 및 객체 리턴
-    // }, [])
     return (
 
     <Map // 지도를 표시할 Container
         center={{
             // 지도의 중심좌표
-            lat: 37.516081,
-            lng: 126.722443,
+            lat: 37.51583698541007,
+            lng: 126.72252151890174,
         }}
         style={{
             // 지도의 크기
             width: "450px",
-            height: "450px",
+            height: "225px",
         }}
-        level={5} // 지도의 확대 레벨
+        level={3} // 지도의 확대 레벨
     >
         <MapMarker // 마커를 생성합니다
             position={{
                 // 마커가 표시될 위치입니다
-                lat: 37.516081,
-                lng: 126.722443,
+                lat: 37.51583698541007,
+                lng: 126.72252151890174,
             }}
-            image={options}
+            image={{
+                src: Lovelocation, // 마커이미지의 주소입니다
+                size: {
+                    width: 45,
+                    height: 45,
+                }
+            }}
         />
     </Map>
     )
