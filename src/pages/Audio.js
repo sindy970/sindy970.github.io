f47caa37245676a49367e837be61407ac722bdff
한승희 <seunghee.han@emotion.co.kr>
2025-03-06 14:59:32 +0900

    배경음악 변경

--- src/pages/Audio.js
+++ src/pages/Audio.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import Music from '../DAY6 데이식스 Welcome to the Show.mp3';
+import Music from '../background.mp3';
 
 const Audioplay = (props) => {
     const audioRef = useRef(new Audio(Music));
@@ -20,19 +20,6 @@ const Audioplay = (props) => {
         }
     };
 
-    // useEffect(() => {
-    //     // 컴포넌트가 렌더링된 후 버튼을 자동으로 클릭
-    //     if (props.clickEvent) {
-    //         toggleAudio();
-    //         // audio.play();
-    //         // setShowNextComponent(true);
-    //     }
-    // }, []);
-    //
-    // const toggleAudio = () => {
-    //     audio.play();
-    // };
-
     return (
         <div>
             <audio ref={audioRef} src={Music} loop style={{background: 'none'}}></audio>
@@ -42,20 +29,3 @@ const Audioplay = (props) => {
 
 };
 export default Audioplay;
-
-// <button onClick={() => {
-//     if (props.clickEvent) {
-//         audioRef.current.play(); // 버튼 클릭 시 음악 재생
-//     }
-// }} >
-// </button>
-
-//
-// <audio
-//
-//     loop // 무한 재생을 위한 loop 속성
-//     autoPlay // 페이지 로드시 자동 재생
-//     style={{background: 'none'}}
-// >
-//     <source src={Music} type="audio/mpeg"/>
-// </audio>
